2333aabee3ec0fcc617fcd2ea9e55ed901d7657d
Argane Samson <argane.samson@gmail.com>
2022-02-08 13:10:54 +0100

    creation tests ParkingSpotDAO testGetNextAvailableSlot et testUpdateParking

---
+++ src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOTest.java
@@ -0,0 +1,43 @@
+package com.parkit.parkingsystem.dao;
+
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ParkingSpotDAOTest {
+
+    private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+    private static ParkingSpotDAO parkingSpotDAO;
+    private static DataBasePrepareService dataBasePrepareService;
+
+    @Mock
+    private ParkingSpot parkingSpot;
+
+    @BeforeAll
+    private static void setUp() {
+        parkingSpotDAO = new ParkingSpotDAO();
+        parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
+        dataBasePrepareService = new DataBasePrepareService();
+    }
+
+    @Test
+    void testGetNextAvailableSlot() {
+        int nextParkingSpot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+        assertEquals(nextParkingSpot, 1);
+    }
+
+    @Test
+    void testUpdateParking() {
+        boolean updateParking = parkingSpotDAO.updateParking(parkingSpot);
+        assertFalse(updateParking);
+    }
+}
