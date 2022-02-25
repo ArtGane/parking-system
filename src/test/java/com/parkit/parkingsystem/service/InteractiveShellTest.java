fe6afb11d01aed82b44578c1d47345380ff85e00
Argane Samson <argane.samson@gmail.com>
2022-02-25 10:35:22 +0100

    création class de test pour interaactiveshell

---
+++ src/test/java/com/parkit/parkingsystem/service/InteractiveShellTest.java
@@ -0,0 +1,12 @@
+package com.parkit.parkingsystem.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InteractiveShellTest {
+
+    @Test
+    void testLoadInterface() {
+    }
+}
